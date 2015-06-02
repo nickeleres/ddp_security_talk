046616969c6e9dc2f8a764bfb3f0a69703200242
Nick <nick.bucheleres@gmail.com>
2015-06-02 13:15:35 -0700

    added login template

--- ddp_security_talk.js
+++ ddp_security_talk.js
@@ -7,21 +7,33 @@ if (Meteor.isClient) {
       console.log('waitOn has run');
       return Meteor.subscribe('users');
       this.next();
+    },
+    onBeforeAction: function(){
+      if(! Meteor.userId()){
+        this.render('loginTemplate');
+      } else {
+        this.next();
+      }
     }
   })
 
   Template.usersTemplate.helpers({
     users: function(){
       return Meteor.users.find();
+    },
+
+    isAdmin: function(){
+      console.log(!!Meteor.users.findOne({_id: Meteor.userId(), admin: true}));
+      return !!Meteor.users.findOne({_id: Meteor.userId(), admin: true});
     }
   });
 }
 
 if (Meteor.isServer) {
     Meteor.publish('users', function(){
-      if(this.userId === '4wnRxRXZSBz6ivvAY'){
+      // if(this.userId === '4wnRxRXZSBz6ivvAY'){
         console.log(this.userId);
         return Meteor.users.find();
-      }
+      // }
     })
 }
